e1298ff05a3532f6b247e4f7855f9f1ea318a72b
Megagig <megagigsoftwaresolutions.com>
2025-02-06 12:49:46 +0100

    Project Setup

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors()); //cors is a middleware that allows or blocks requests based on the origin of the request. It's a security feature to prevent unauthorized access to your server.
+app.use(helmet()); //helmet is a security middleware that helps you protect your app from some well-known web vulnerabilities by setting HTTP headers appropriately.
+app.use(morgan('dev')); // morgan is a middleware that logs HTTP requests. It's a great tool for debugging and seeing what requests are coming in.
+app.use(express.json()); // express.json() is a middleware that parses incoming requests with JSON payloads.
+
+//routes
+app.get('/', (req, res) => {
+  res.send('Hello World from the backend');
+});
+
+//start server
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
