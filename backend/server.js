c404d5a181941a2f307c2b710227c8b3c586e741
Megagig <megagigsoftwaresolutions.com>
2025-02-07 20:38:57 +0100

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import path from 'path';
 import productRoutes from './routes/productRoutes.js';
 import { sql } from './config/dbConnect.js';
 import { aj } from './lib/arcjet.js';
@@ -11,10 +12,15 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 // Middleware
 app.use(cors()); //cors is a middleware that allows or blocks requests based on the origin of the request. It's a security feature to prevent unauthorized access to your server.
-app.use(helmet()); //helmet is a security middleware that helps you protect your app from some well-known web vulnerabilities by setting HTTP headers appropriately.
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+); //helmet is a security middleware that helps you protect your app from some well-known web vulnerabilities by setting HTTP headers appropriately.
 app.use(morgan('dev')); // morgan is a middleware that logs HTTP requests. It's a great tool for debugging and seeing what requests are coming in.
 app.use(express.json()); // express.json() is a middleware that parses incoming requests with JSON payloads.
 
@@ -55,6 +61,16 @@ app.use(async (req, res, next) => {
 
 //routes
 app.use('/api/v1/products', productRoutes);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // server our react app
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
+}
 
 //Initialize Database
 async function dbConnect() {
