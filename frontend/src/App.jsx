d60a3764b7b02a67e3e04bd74faef19c8723068d
Megagig <megagigsoftwaresolutions.com>
2025-02-07 12:48:05 +0100

    Finish the Navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,12 @@ import HomePage from './pages/HomePage.jsx';
 import ProductPage from './pages/ProductPage.jsx';
 // import ProductDetailPage from './pages/ProductDetailPage.jsx';
 import Navbar from './components/Navbar';
+import Resources from './pages/Resources';
+import Company from './pages/Company';
+import Contact from './pages/Contact.jsx';
+import About from './pages/About';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -11,6 +17,13 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<ProductPage />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
         {/* <Route path="/product/:id" element={<ProductDetailPage />} /> */}
       </Routes>
     </div>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,68 @@
-function Navbar() {
-  return <div>navbar</div>;
-}
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+
+const Navbar = () => {
+  // State to manage the navbar's visibility
+  const [nav, setNav] = useState(false);
+
+  // Toggle function to handle the navbar's display
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  // Array containing navigation items
+  const navItems = [
+    { id: 1, text: 'Company', path: '/company' },
+    { id: 2, text: 'Resources', path: '/resources' },
+    { id: 3, text: 'About', path: '/about' },
+    { id: 4, text: 'Contact', path: '/contact' },
+    { id: 5, text: 'Register', path: '/register' },
+  ];
+
+  return (
+    <div className="bg-black flex justify-between items-center h-24 w-full px-4 text-white">
+      <Link to="/">
+        {' '}
+        <h1 className="w-full text-3xl font-bold text-[#00df9a]">
+          MEGAGIG
+        </h1>{' '}
+      </Link>
+
+      <ul className="hidden md:flex">
+        {navItems.map((item) => (
+          <li
+            key={item.id}
+            className="p-4 hover:bg-[#00df9a] rounded-xl m-2 cursor-pointer duration-300 hover:text-black"
+          >
+            <Link to={item.path}>{item.text}</Link>
+          </li>
+        ))}
+      </ul>
+
+      <div onClick={handleNav} className="block md:hidden">
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+
+      <ul
+        className={
+          nav
+            ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
+            : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
+        }
+      >
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">REACT.</h1>
+        {navItems.map((item) => (
+          <li
+            key={item.id}
+            className="p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600"
+          >
+            <Link to={item.path}>{item.text}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Navbar;
